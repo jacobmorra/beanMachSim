94df66cfc81267e53ba01c0f39386f7fcc661ef3
100395426 <jacob.morra@uoit.net>
2017-02-23 02:28:42 -0500

    add histogram output

--- beanMachSim.py
+++ beanMachSim.py
@@ -1,4 +1,7 @@
 import random as r
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 class beanMachSim():
     def __init__(self, numBalls):
@@ -40,8 +43,17 @@ class beanMachSim():
 
 
 def main():
-    q = beanMachSim(1000)
+    q = beanMachSim(10000)
     q.dropAllBalls()
     print q.binContents
+    xbin = [0,1,2,3,4,5,6,7,8,9]
 
+    print q.binContents[1]
+    import matplotlib.pyplot as plt
+    plt.bar(range(0, 10), q.binContents)
+    plt.show()
+    #plt.hist(xbin, bins=10, weight=q.binContents)
+    ## plt.hist([q.binContents[0],q.binContents[1],q.binContents[2],q.binContents[3], q.binContents[4], q.binContents[5], q.binContents[6], q.binContents[7],q.binContents[8], q.binContents[9]], bins=[0,1,2,3,4,5,6,7,8,9], weights=[1,1,1,1,1,1,1,1,1,1])
+    #plt.hist([1, 11, 21, 31, 41, 51, 61, 71, 81, 91], bins=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], weights=[0,1,1,1,1,1,1,1,1,1])
+    plt.show()
 if __name__ == "__main__": main()
