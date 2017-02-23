d11285a853422b393eb1bb086a7851125840c69f
100395426 <jacob.morra@uoit.net>
2017-02-22 22:47:52 -0500

    Create beanMachSim class, add functions for displaying board and determining probabilities.

---
+++ beanMachSim.py
@@ -0,0 +1,14 @@
+class beanMachSim():
+    def __init__(self, numBalls, numRows, numBins):
+        self.numBalls = numBalls
+        self.numRows = numRows
+        self.numBins = numBins
+
+    def display(self):
+
+    def probLand(self):
+        return
+
+def main():
+
+if __name__ == "__main__": main()
