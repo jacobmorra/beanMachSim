e84e48a314be920de4715942002a046a18326f89
100395426 <jacob.morra@uoit.net>
2017-02-24 02:27:45 -0500

    added modularity, changed hardcoded values to reference values dependent on a constant (numRows)

--- beanMachSim.py
+++ beanMachSim.py
@@ -3,16 +3,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
+#have n bins, n-1 pins at bottom row, then n-2, n-3...
+
 class beanMachSim():
     def __init__(self, numBalls):
         self.numBalls = numBalls
-        self.numRows = 9
-        self.numBins = 10
-        self.binContents = [0,0,0,0,0,0,0,0,0,0]
+        self.numRows = 20
+        self.numBins = self.numRows+1
+        self.binContents = np.zeros((self.numBins,), dtype=np.int)
         self.ballPos = 0
 
+    def theorResult(self):
+        return True
+
     def moveRight(self):
-        p = r.random()
+        p = np.random.rand()
         if p > 0.5 and p < 1:
             #print p
             return True
@@ -21,14 +26,16 @@ class beanMachSim():
             return False
 
     def dropBall(self):
-        self.ballPos = 4 #reset ball pos to middle b/w 0 and 9
-        for i in range(self.numRows):
+        self.ballPos = (self.numBins)/2 #reset ball pos to middle b/w 0 and 9
+        print "start @ middle: ", self.ballPos
+        for i in range(self.numRows+1):
+            print "row: ", i
             right = self.moveRight()
             if right is True:
-                if self.ballPos == 9:
+                if self.ballPos == self.numBins-1:
                     print "ball @ 9"
                     pass
-                elif self.ballPos < 9:
+                elif self.ballPos < self.numBins-1:
                     print "move right"
                     self.ballPos += 1
             if right is False:
@@ -53,14 +60,13 @@ def main():
     q = beanMachSim(10000)
     q.dropAllBalls()
     print q.binContents
-    xbin = [0,1,2,3,4,5,6,7,8,9]
 
-    print q.binContents[1]
+
     import matplotlib.pyplot as plt
-    plt.bar(range(0, 10), q.binContents)
+    plt.bar(range(0, q.numBins), q.binContents)
     plt.show()
-    #plt.hist(xbin, bins=10, weight=q.binContents)
-    ## plt.hist([q.binContents[0],q.binContents[1],q.binContents[2],q.binContents[3], q.binContents[4], q.binContents[5], q.binContents[6], q.binContents[7],q.binContents[8], q.binContents[9]], bins=[0,1,2,3,4,5,6,7,8,9], weights=[1,1,1,1,1,1,1,1,1,1])
-    #plt.hist([1, 11, 21, 31, 41, 51, 61, 71, 81, 91], bins=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], weights=[0,1,1,1,1,1,1,1,1,1])
-    plt.show()
+    buckets = [[0 for col in range(5)] for row in range(10)]
+    print buckets
+
+
 if __name__ == "__main__": main()
